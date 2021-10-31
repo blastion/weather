6cb860e2b8fe8f3e3f519b21bd6d8ecd4952a9a7
blastion <93491792+blastion@users.noreply.github.com>
2021-10-31 17:25:32 -0300

    add temp

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-import os
+import requests
 
 
-print('test')
+class getWeather:
+    def __init__(self, city:str) -> None:
+        self.city = city
+        self.api_key = '2148f0586866bb176be0d3f7b7bexxxxxxxxxxxxx'
+    def weather(self):
+        url = 'http://api.openweathermap.org/data/2.5/weather'
+        params = {'APPID': self.api_key, 'q': self.city, 'units': 'metric'}
+        response = requests.get(url, params=params)
+        data = response.json()
+        temp = data['main']['temp']
+        return temp
+
