e7f25928af1484c205e156582cd621e5db392bba
blastion <93491792+blastion@users.noreply.github.com>
2021-10-31 17:25:50 -0300

    add webhook

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import requests
 class getWeather:
     def __init__(self, city:str) -> None:
         self.city = city
-        self.api_key = '2148f0586866bb176be0d3f7b7bexxxxxxxxxxxxx'
+        self.api_key = ''
+
     def weather(self):
         url = 'http://api.openweathermap.org/data/2.5/weather'
         params = {'APPID': self.api_key, 'q': self.city, 'units': 'metric'}
@@ -13,3 +14,16 @@ class getWeather:
         temp = data['main']['temp']
         return temp
 
+class WebHook:
+    def __init__(self, url:str, temp:float) -> None:
+        self.url = url
+        self.temp = temp
+
+    def send(self):
+        data = {"value1": str(self.temp)}
+        print(data)
+        requests.post(self.url, json=data)
+
+sao_paulo = getWeather('São Paulo')
+tweet = WebHook('https://maker.ifttt.com/trigger/t/with/key/beAZ5WtBpub2VZ6TM24TWW', sao_paulo.weather())
+tweet.send()
